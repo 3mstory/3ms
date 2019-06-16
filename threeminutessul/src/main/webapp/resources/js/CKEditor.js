1db125b03e4a975e80b98d463e92c0c221a0b17b
simsimjae <simsimjae@naver.com>
2019-06-16 15:32:54 +0900

    글작성

---
+++ threeminutessul/src/main/webapp/resources/js/CKEditor.js
@@ -0,0 +1,73 @@
+class MyUploadAdapter {
+  constructor(loader) {
+    this.loader = loader;
+  }
+
+  upload() {
+    return this.loader.file
+      .then(file => new Promise((resolve, reject) => {
+        this._initRequest();
+        this._initListeners(resolve, reject, file);
+        this._sendRequest(file);
+      }));
+  }
+
+  abort() {
+    if (this.xhr) {
+      this.xhr.abort();
+    }
+  }
+
+  _initRequest() {
+    const xhr = this.xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://example.com/image/upload/path', true);
+    xhr.responseType = 'json';
+  }
+
+  _initListeners(resolve, reject, file) {
+    const xhr = this.xhr;
+    const loader = this.loader;
+    const genericErrorText = `${ file.name } 은 업로드 불가능 합니다.`;
+
+    xhr.addEventListener('error', () => reject(genericErrorText));
+    xhr.addEventListener('abort', () => reject());
+    xhr.addEventListener('load', () => {
+      const response = xhr.response;
+      if (!response || response.error) {
+        return reject(response && response.error ? response.error.message : genericErrorText);
+      }
+      resolve({
+        default: response.url
+      });
+    });
+    if (xhr.upload) {
+      xhr.upload.addEventListener('progress', evt => {
+        if (evt.lengthComputable) {
+          loader.uploadTotal = evt.total;
+          loader.uploaded = evt.loaded;
+        }
+      });
+    }
+  }
+
+  _sendRequest(file) {
+    const data = new FormData();
+    data.append('upload', file);
+    this.xhr.send(data);
+  }
+}
+
+function MyCustomUploadAdapterPlugin(editor) {
+  editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+    return new MyUploadAdapter(loader);
+  };
+}
+
+ClassicEditor
+  .create(document.querySelector('#write_cont'), {
+    extraPlugins: [MyCustomUploadAdapterPlugin],
+    toolbar: ['bold', 'ImageUpload']
+  })
+  .catch(error => {
+    console.error(error);
+  });
