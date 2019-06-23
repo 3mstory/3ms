6c73b514ebde6625ca5d384dc318b2ce2a0ed520
simsimjae <simsimjae@naver.com>
2019-06-23 16:17:07 +0900

    스크롤 js 수

--- threeminutessul/src/main/webapp/resources/js/common.js
+++ threeminutessul/src/main/webapp/resources/js/common.js
@@ -4,13 +4,13 @@ $(function () {
     var text = $('#card_write').val(),
       matches = text.match(/\n/g),
       breaks = matches ? matches.length : 2;
-    console.log(breaks);
     $('#card_write').attr('rows', breaks + 2);
   });
 
   $('.card-collapse').on('shown.bs.collapse',function(){
     var topoffset = $('.navbar').outerHeight();
     var target = $(this).parents('.card');
+    var tgmargin = target.css('margin-top').substr(-2,0);
     $('html, body').animate({
       scrollTop: target.offset().top - topoffset - tgmargin
     },0);
